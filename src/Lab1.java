07ec2a80ff6ee06d98a2e9d6feffd1faa76b6c81
Ekaterina Sivkova <Katasivkova352@gmail.com>
2025-09-20 11:40:21 +0500

    Initial commit

---
+++ src/Lab1.java
@@ -0,0 +1,201 @@
+public class Lab1 {
+    //1.1
+    public double fraction (double x) {
+        int myInt = (int) x;
+        return x - myInt;
+    }
+    //1.3
+    public int charToNum (char x) {
+        return (int) x - 48;
+    }
+    //1.5
+    public boolean is2Digits (int x) {
+        if ((x > 9 && x < 100) || (x > -100 && x < -9)) {
+            return true;
+        }
+        return false;
+    }
+    //1.7
+    public boolean isInRange (int a, int b, int num) {
+        if ((num >= a && num <= b) || (num <= a && num >= b)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    //1.9
+    public boolean isEqual(int a, int b, int c) {
+        if (a == b && b == c) {
+            return true;
+        }
+        return false;
+    }
+    //2.1
+    public int abs (int x) {
+        if (x >= 0) {
+            return x;
+        }
+        else {
+            return -x;
+        }
+    }
+    //2.3
+    public boolean is35 (int x) {
+        if (x % 3 == 0 && x % 5 == 0) {
+            return false;
+        }
+        else if (x % 3 == 0 || x % 5 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    //2.5
+    public int max3 (int x, int y, int z) {
+        int max = x;
+        if (y > max) {
+            max = y;
+        }
+        if (z > max) {
+            max = z;
+        }
+        return max;
+    }
+    //2.7
+    public int sum2 (int x, int y) {
+        if (x + y >= 10 && x + y <= 19) {
+            return 20;
+        }
+        return x + y;
+    }
+    //2.9
+    public String day (int x) {
+        switch(x) {
+            case 1:
+                return "Понедельник";
+            case 2:
+                return "Вторник";
+            case 3:
+                return "Среда";
+            case 4:
+                return "Четверг";
+            case 5:
+                return "Пятница";
+            case 6:
+                return "Суббота";
+            case 7:
+                return "Воскресенье";
+            default:
+                return "Это не день недели";
+        }
+    }
+    //3.1
+    public String listNums (int x) {
+        String str = "";
+        for (int i = 0; i<=x ; i++) {
+            str = str + Integer.toString(i) + " ";
+        }
+        return str;
+    }
+    //3.3
+    public String chet (int x) {
+        String str = "";
+        int i = 0;
+        while (i<=x) {
+            str = str + Integer.toString(i) + " ";
+            i += 2;
+        }
+        return str;
+    }
+    //3.5
+    public int numLen (long x) {
+        int count = 0;
+        do {
+            count++;
+            x /= 10;
+        } while (x != 0);
+        return count;
+    }
+    //3.7
+    public void square (int x) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    //3.9
+    public void rightTriangle (int x) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 0; j < x-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    //4.1
+    public int findFirst (int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+    //4.3
+    public int maxAbs (int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(Math.abs(arr[i]) > Math.abs(max)) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    //4.5
+    public int[] add (int[] arr, int[] ins, int pos) {
+        int[] res = new int[arr.length + ins.length];
+        int index = 0;
+        for (int i = 0; i < pos; i++) {
+            res[index++] = arr[i];
+        }
+        for (int j = 0; j < ins.length; j++) {
+            res[index++] = ins[j];
+        }
+        for (int i = pos; i < arr.length; i++) {
+            res[index++] = arr[i];
+        }
+        return res;
+    }
+    //4.7
+    public int[] reverseBack (int[] arr) {
+        int[] res = new int[arr.length];
+        int index = 0;
+        for (int j = arr.length - 1; j >= 0; j--) {
+            res[index++] = arr[j];
+        }
+        return res;
+    }
+    //4.9
+    public int[] findAll (int[] arr, int x) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                count++;
+            }
+        }
+        int[] res = new int[count];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                res[index++] = i;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,324 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Lab1 l = new Lab1();
+
+//        System.out.println("Задача 1.1");
+//        System.out.println("Введите вещественное число.");
+//        double x1_1 = scanner.nextDouble();
+//
+//        double y1_1 = l.fraction(x1_1);
+//        System.out.println("Дробная часть введённого числа: " + y1_1);
+
+//        System.out.println("Задача 1.3");
+//        System.out.println("Введите символ (одну из цифр).");
+//        char x1_3 = scanner.next().charAt(0);
+//
+//        if (!xIsChar(x1_3)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        int y1_3 = l.charToNum(x1_3);
+//        System.out.println("Символ преобразованный в число: " + y1_3);
+
+//        System.out.println("Задача 1.5");
+//        System.out.println("Введите целое число.");
+//        int x1_5 = scanner.nextInt();
+//
+//        boolean y1_5 = l.is2Digits(x1_5);
+//        System.out.println("Число двухзначное? " + y1_5);
+
+//        System.out.println("Задача 1.7");
+//        System.out.println("Введите первую границу диапазона.");
+//        int a = scanner.nextInt();
+//        System.out.println("Введите вторую границу диапазона.");
+//        int b = scanner.nextInt();
+//        System.out.println("Введите целое число.");
+//        int num = scanner.nextInt();
+//
+//        boolean y1_7 = l.isInRange(a, b, num);
+//        System.out.println("Число принадлежит указанному диапазону? " + y1_7);
+
+//        System.out.println("Задача 1.9");
+//        System.out.println("Введите первое число.");
+//        int a = scanner.nextInt();
+//        System.out.println("Введите второе число.");
+//        int b = scanner.nextInt();
+//        System.out.println("Введите третье число.");
+//        int c = scanner.nextInt();
+//
+//        boolean y1_9 = l.isEqual(a, b, c);
+//        System.out.println("Три числа равны? " + y1_9);
+
+//        System.out.println("Задача 2.1");
+//        System.out.println("Введите целое число.");
+//        int x2_1 = scanner.nextInt();
+//
+//        int y2_1 = l.abs(x2_1);
+//        System.out.println("Модуль введённого числа: " + y2_1);
+
+//        System.out.println("Задача 2.3");
+//        System.out.println("Введите целое число.");
+//        int x2_3 = scanner.nextInt();
+//
+//        boolean y2_3 = l.is35(x2_3);
+//        System.out.println("Число  делится нацело на 3 или на 5? " + y2_3);
+
+//        System.out.println("Задача 2.5");
+//        System.out.println("Введите первое число.");
+//        int x2_5 = scanner.nextInt();
+//        System.out.println("Введите второе число.");
+//        int y2_5 = scanner.nextInt();
+//        System.out.println("Введите третье число.");
+//        int z2_5 = scanner.nextInt();
+//
+//        int res2_5 = l.max3(x2_5, y2_5, z2_5);
+//        System.out.println("Максимальное из чисел: " + res2_5);
+
+//        System.out.println("Задача 2.7");
+//        System.out.println("Введите первое число.");
+//        int x2_7 = scanner.nextInt();
+//        System.out.println("Введите второе число.");
+//        int y2_7 = scanner.nextInt();
+//
+//        int res2_7 = l.sum2(x2_7, y2_7);
+//        System.out.println("Сумма двух чисел: " + res2_7);
+
+//        System.out.println("Задача 2.9");
+//        System.out.println("Введите целое число.");
+//        int x2_9 = scanner.nextInt();
+//
+//        String y2_9 = l.day(x2_9);
+//        System.out.println("День недели: " + y2_9);
+
+//        System.out.println("Задача 3.1");
+//        System.out.println("Введите целое число.");
+//        int x3_1 = scanner.nextInt();
+//
+//        if (!xIsNatural(x3_1)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        String y3_1 = l.listNums(x3_1);
+//        System.out.println("Результат: " + y3_1);
+
+//        System.out.println("Задача 3.3");
+//        System.out.println("Введите целое число.");
+//        int x3_3 = scanner.nextInt();
+//
+//        if(!xIsNatural(x3_3)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        String y3_3 = l.chet(x3_3);
+//        System.out.println("Результат: " + y3_3);
+
+//        System.out.println("Задача 3.5");
+//        System.out.println("Введите целое число.");
+//        int x3_5 = scanner.nextInt();
+//
+//        if (!xIsNatural(x3_5)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        int y3_5 = l.numLen(x3_5);
+//        System.out.println("Количество знаков в числе: " + y3_5);
+
+//        System.out.println("Задача 3.7");
+//        System.out.println("Введите целое число.");
+//        int x3_7 = scanner.nextInt();
+//
+//        if (!xIsNatural(x3_7)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        l.square(x3_7);
+
+//        System.out.println("Задача 3.9");
+//        System.out.println("Введите целое число.");
+//        int x3_9 = scanner.nextInt();
+//
+//        if (!xIsNatural(x3_9)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        l.rightTriangle(x3_9);
+
+//        System.out.println("Задача 4.1");
+//        System.out.println("Введите целое число (количество элементов массива).");
+//        int n4_1 = scanner.nextInt();
+//
+//        if (!xIsNatural(n4_1)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        if (!MasSize(n4_1)) {
+//            System.out.println("Массив пустой.");
+//            return;
+//        }
+//
+//        int[] arr = new int[n4_1];
+//        for (int i = 0; i < n4_1; i++) {
+//            System.out.println("Введите " + (i + 1) + " элемент массива.");
+//            arr[i] = scanner.nextInt();
+//        }
+//
+//        System.out.println("Введите чсило x, индекс которого необходимо найти.");
+//        int x4_1 = scanner.nextInt();
+//
+//        int y4_1 = l.findFirst(arr, x4_1);
+//        System.out.println("Индекс первого вхождения числа " + x4_1 + " в массив: " + y4_1);
+
+//        System.out.println("Задача 4.3");
+//        System.out.println("Введите целое число (количество элементов массива).");
+//        int n4_3 = scanner.nextInt();
+//
+//        if (!xIsNatural(n4_3)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        if (!MasSize(n4_3)) {
+//            System.out.println("Массив пустой.");
+//            return;
+//        }
+//
+//        int[] arr = new int[n4_3];
+//        for (int i = 0; i < n4_3; i++) {
+//            System.out.println("Введите " + (i + 1) + " элемент массива.");
+//            arr[i] = scanner.nextInt();
+//        }
+//
+//        int y4_3 = l.maxAbs(arr);
+//        System.out.println("Наибольшее по модулю значение массива: " + y4_3);
+
+//        System.out.println("Задача 4.5");
+//        System.out.println("Введите целое число (количество элементов массива arr).");
+//        int n4_5 = scanner.nextInt();
+//
+//        if (!xIsNatural(n4_5)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        if (!MasSize(n4_5)) {
+//            System.out.println("Массив arr пустой.");
+//            return;
+//        }
+//
+//        int[] arr = new int[n4_5];
+//        for (int i = 0; i < n4_5; i++) {
+//            System.out.println("Введите " + (i + 1) + " элемент массива arr.");
+//            arr[i] = scanner.nextInt();
+//        }
+//
+//        System.out.println("Введите целое число (количество элементов массива ins).");
+//        int m4_5 = scanner.nextInt();
+//
+//        if (!xIsNatural(m4_5)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        if (!MasSize(m4_5)) {
+//            System.out.println("Массив ins пустой.");
+//            return;
+//        }
+//
+//        int[] ins = new int[m4_5];
+//        for (int i = 0; i < m4_5; i++) {
+//            System.out.println("Введите " + (i + 1) + " элемент массива ins.");
+//            ins[i] = scanner.nextInt();
+//        }
+//
+//        System.out.println("Введите позицию pos.");
+//        int pos = scanner.nextInt();
+//
+//        if (!xIsNatural(pos)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        int[] y4_5 = l.add(arr, ins, pos);
+//        System.out.print("Новый массив:  ");
+//        for (int i = 0; i < y4_5.length; i++) {
+//            System.out.print(y4_5[i] + " ");
+//        }
+
+//        System.out.println("Задача 4.7");
+//        System.out.println("Введите целое число (количество элементов массива).");
+//        int n4_7 = scanner.nextInt();
+//
+//        if (!xIsNatural(n4_7)) {
+//            System.out.println("Ошибка ввода.");
+//            return;
+//        }
+//
+//        if (!MasSize(n4_7)) {
+//            System.out.println("Массив пустой.");
+//            return;
+//        }
+//
+//        int[] arr = new int[n4_7];
+//        for (int i = 0; i < n4_7; i++) {
+//            System.out.println("Введите " + (i + 1) + " элемент массива.");
+//            arr[i] = scanner.nextInt();
+//        }
+//
+//        int[] y4_7 = l.reverseBack(arr);
+//        System.out.print("Новый массив:  ");
+//        for (int i = 0; i < y4_7.length; i++) {
+//            System.out.print(y4_7[i] + " ");
+//        }
+
+        System.out.println("Задача 4.9");
+        System.out.println("Введите целое число (количество элементов массива).");
+        int n4_9 = scanner.nextInt();
+
+        if (!xIsNatural(n4_9)) {
+            System.out.println("Ошибка ввода.");
+            return;
+        }
+
+        if (!MasSize(n4_9)) {
+            System.out.println("Массив пустой.");
+            return;
+        }
+
+        int[] arr = new int[n4_9];
+        for (int i = 0; i < n4_9; i++) {
+            System.out.println("Введите " + (i + 1) + " элемент массива.");
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Введите число x, индексы которого необходимо найти.");
+        int x4_9 = scanner.nextInt();
+
+        int[] y4_9 = l.findAll(arr, x4_9);
+        System.out.print("Индексы вхождения числа " + x4_9 + " в массив:  ");
+        for (int i = 0; i < y4_9.length; i++) {
+            System.out.print(y4_9[i] + " ");
+        }
+
+        scanner.close();
+    }
+    private static boolean xIsChar(char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+    private static boolean xIsNatural(int x) {
+        return x >= 0;
+    }
+    private static boolean MasSize(int n) {
+        return n != 0;
+    }
+}
+
